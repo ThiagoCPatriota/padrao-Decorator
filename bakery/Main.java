9f496c4678ca48a1e69077c7fe8f8789cbf76006
Thiago Patriota <thiagosilvapatriota@gmail.com>
2026-09-21 19:03:44 -0300

    feat: simular pedido final da padaria

--- bakery/Main.java
+++ bakery/Main.java
@@ -3,12 +3,33 @@
  */
 public class Main {
     public static void main(String[] args) {
-        // Create the order
         Order order = new Order();
-        order.addCake(new VanillaCake());
-        order.addCake(new ChocolateCake());
 
-        // Print the order
+        order.addCake(new ChocolateCake());
+        order.addCake(new SayingCake(new VanillaCake(), "PLAIN!"));
+        order.addCake(new SayingCake(new SprinklesCake(new VanillaCake()), "FANCY!"));
+        order.addCake(new SayingCake(
+                new SayingCake(
+                        new SprinklesCake(
+                                new SprinklesCake(
+                                        new MultiLayeredCake(new StrawberryCake())
+                                )
+                        )
+                ),
+                "One of"
+        ));
+
+        order.addCake(new SayingCake(
+                new SayingCake(
+                        new SprinklesCake(
+                                new SprinklesCake(
+                                        new MultiLayeredCake(new StrawberryCake())
+                                )
+                        )
+                ),
+                "EVERYTHING"
+        ));
+
         order.printOrder();
     }
 }
